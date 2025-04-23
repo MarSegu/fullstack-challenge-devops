6e4596d2484a8c8ed7d957ac955b7cff575bd4e9
MarSegu <mariosegura0904@gmail.com>
2025-04-22 20:05:03 -0500

    modified:   terraform/modules/azure-webapp/azure-resources-webapp.tf

--- terraform/modules/azure-webapp/azure-resources-webapp.tf
+++ terraform/modules/azure-webapp/azure-resources-webapp.tf
@@ -54,7 +54,7 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
   log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
 
   metric {
-    category = "Transaction"
+    category = "allLogs"
     enabled = true
   }
 
